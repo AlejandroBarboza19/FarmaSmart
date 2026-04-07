1024733fa7dbd414d93fa710d6755b8355bcf1be
Randy <alejobarboza54@gmail.com>
2026-04-07 12:08:58 -0500

    setup: backend inicial con FastAPI y configuración de entorno

---
+++ backend/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app =  FastAPI(title="FastAPI Backend", description="A simple FastAPI backend for demonstration purposes.", version="1.0.0")
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a FarmaSmart API!"}
